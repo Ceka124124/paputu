0a9b16fb8bc6d734ac7d5ffcbf52187ff90c0412
Ceka124124 <rowqa08@gmail.com>
2025-04-11 23:24:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,44 +13,59 @@ let rooms = {};
 io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
-    socket.on('joinRoom', ({ roomId }) => {
-        if (!rooms[roomId]) rooms[roomId] = new Array(6).fill(null);
+    socket.on('joinRoom', ({ roomId, userName }) => {
+        // Oda yoksa, oda oluşturuluyor
+        if (!rooms[roomId]) {
+            rooms[roomId] = {
+                seats: new Array(6).fill(null),
+                users: {}
+            };
+        }
+
+        // Kullanıcıya oda ve adı ile birlikte katılım sağlanıyor
+        rooms[roomId].users[socket.id] = userName;
         socket.join(roomId);
-        io.to(roomId).emit('userJoined', { allSeats: rooms[roomId] });
+        io.to(roomId).emit('userJoined', { allSeats: rooms[roomId].seats });
+
+        console.log(`${userName} katıldı: ${socket.id}`);
     });
 
     socket.on('joinSeat', ({ seatId, userId }) => {
         const roomId = 'testRoom';
-        if (!rooms[roomId]) rooms[roomId] = new Array(6).fill(null);
+        if (!rooms[roomId]) rooms[roomId] = { seats: new Array(6).fill(null) };
 
-        if (rooms[roomId][seatId] && rooms[roomId][seatId] !== userId) {
+        // Koltuk doluysa uyarı gönderiyoruz
+        if (rooms[roomId].seats[seatId] && rooms[roomId].seats[seatId] !== userId) {
             socket.emit('seatTaken', { seatId });
             return;
         }
 
-        for (let i = 0; i < rooms[roomId].length; i++) {
-            if (rooms[roomId][i] === userId) {
-                rooms[roomId][i] = null;
+        // Koltuktan çıkıp yeni koltuğa geçiş işlemi
+        for (let i = 0; i < rooms[roomId].seats.length; i++) {
+            if (rooms[roomId].seats[i] === userId) {
+                rooms[roomId].seats[i] = null;
             }
         }
 
-        rooms[roomId][seatId] = userId;
-        io.to(roomId).emit('userJoined', { allSeats: rooms[roomId] });
+        rooms[roomId].seats[seatId] = userId;
+        io.to(roomId).emit('userJoined', { allSeats: rooms[roomId].seats });
     });
 
     socket.on('leaveRoom', ({ userId }) => {
         const roomId = 'testRoom';
         if (rooms[roomId]) {
-            rooms[roomId] = rooms[roomId].map(uid => uid === userId ? null : uid);
-            io.to(roomId).emit('userJoined', { allSeats: rooms[roomId] });
+            rooms[roomId].seats = rooms[roomId].seats.map(uid => uid === userId ? null : uid);
+            delete rooms[roomId].users[userId];
+            io.to(roomId).emit('userJoined', { allSeats: rooms[roomId].seats });
         }
     });
 
     socket.on('disconnect', () => {
         const roomId = 'testRoom';
         if (rooms[roomId]) {
-            rooms[roomId] = rooms[roomId].map(uid => uid === socket.id ? null : uid);
-            io.to(roomId).emit('userJoined', { allSeats: rooms[roomId] });
+            rooms[roomId].seats = rooms[roomId].seats.map(uid => uid === socket.id ? null : uid);
+            delete rooms[roomId].users[socket.id];
+            io.to(roomId).emit('userJoined', { allSeats: rooms[roomId].seats });
         }
         console.log('User disconnected:', socket.id);
     });
