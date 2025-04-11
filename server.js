65f6d1b079ce6d47bba2207329deb1486dbccc1a
Ceka124124 <rowqa08@gmail.com>
2025-04-11 22:06:18 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -15,28 +14,44 @@ io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
 
     socket.on('joinRoom', ({ roomId }) => {
-        if (!rooms[roomId]) rooms[roomId] = [];
+        if (!rooms[roomId]) rooms[roomId] = new Array(6).fill(null);
         socket.join(roomId);
+        io.to(roomId).emit('userJoined', { allSeats: rooms[roomId] });
     });
 
     socket.on('joinSeat', ({ seatId, userId }) => {
         const roomId = 'testRoom';
-        if (rooms[roomId][seatId]) {
+        if (!rooms[roomId]) rooms[roomId] = new Array(6).fill(null);
+
+        if (rooms[roomId][seatId] && rooms[roomId][seatId] !== userId) {
             socket.emit('seatTaken', { seatId });
-        } else {
-            rooms[roomId][seatId] = userId;
-            io.to(roomId).emit('userJoined', { userId, seatId });
+            return;
         }
+
+        for (let i = 0; i < rooms[roomId].length; i++) {
+            if (rooms[roomId][i] === userId) {
+                rooms[roomId][i] = null;
+            }
+        }
+
+        rooms[roomId][seatId] = userId;
+        io.to(roomId).emit('userJoined', { allSeats: rooms[roomId] });
     });
 
     socket.on('leaveRoom', ({ userId }) => {
         const roomId = 'testRoom';
         if (rooms[roomId]) {
             rooms[roomId] = rooms[roomId].map(uid => uid === userId ? null : uid);
+            io.to(roomId).emit('userJoined', { allSeats: rooms[roomId] });
         }
     });
 
     socket.on('disconnect', () => {
+        const roomId = 'testRoom';
+        if (rooms[roomId]) {
+            rooms[roomId] = rooms[roomId].map(uid => uid === socket.id ? null : uid);
+            io.to(roomId).emit('userJoined', { allSeats: rooms[roomId] });
+        }
         console.log('User disconnected:', socket.id);
     });
 });
